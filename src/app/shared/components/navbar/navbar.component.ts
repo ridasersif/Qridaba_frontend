bfb7086204728501c59a549ba8262f4f76100b81
ridasersif <ridasersif1@gmail.com>
2026-04-05 12:19:46 +0100

    feat: implement UI components for item details, booking list, navbar, favorites, and home pages

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, Router } from '@angular/router';
 import { AuthService } from '../../../core/services/auth.service';
@@ -10,6 +10,7 @@ import { AuthService } from '../../../core/services/auth.service';
   templateUrl: './navbar.component.html'
 })
 export class NavbarComponent {
+  @Input() isDark = false;
   authService = inject(AuthService);
   router = inject(Router);
 
